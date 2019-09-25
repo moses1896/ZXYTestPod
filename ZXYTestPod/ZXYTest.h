dbb1afa62d09f4195d160f8ba715fbf727b0b5ce
Moses <moses@itcom888.com>
2019-09-25 10:46:20 +0800

    test

---
+++ ZXYTestPod/ZXYTest.h
@@ -0,0 +1,17 @@
+//
+//  ZXYTest.h
+//  MLiveVideo
+//
+//  Created by mac on 2019/9/25.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZXYTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZXYTestPod/ZXYTest.m
@@ -0,0 +1,13 @@
+//
+//  ZXYTest.m
+//  MLiveVideo
+//
+//  Created by mac on 2019/9/25.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "ZXYTest.h"
+
+@implementation ZXYTest
+
+@end
